70b668228564717c39b9702f4e3f48b9ac12e266
Yoshimasa Niwa <niw@niw.at>
2021-01-04 18:36:38 -0800

    Update examples.

--- Examples/Example/Sources/Command.swift
+++ Examples/Example/Sources/Command.swift
@@ -14,9 +14,42 @@ struct Command: ParsableCommand {
     @Option(name: .shortAndLong, help: "Path to the GraphQL file")
     var file: String
 
+    enum Action: EnumerableFlag {
+        case parse
+        case removeDirectives
+
+        static func help(for value: Command.Action) -> ArgumentHelp? {
+            switch value {
+            case .parse:
+                return "Parse given GraphQL file and print each defintion"
+            case .removeDirectives:
+                return "Rewrite given GraphQL file and print the one without any directives"
+            }
+        }
+    }
+
+    @Flag(help: "Action for the GraphQL file")
+    var action: Action
+
     mutating func run() throws {
         let source = try Source(atPath: file)
         let document = try Document.parsing(source)
-        print(document)
+
+        switch action {
+        case .parse:
+            for definition in document.definitions {
+                print(definition)
+            }
+        case .removeDirectives:
+            let result = try document.rewrite { rewritable in
+                switch rewritable.visitable {
+                case is Directive:
+                    return ""
+                default:
+                    return nil
+                }
+            }
+            print(result ?? source.string)
+        }
     }
 }
